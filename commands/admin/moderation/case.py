d1f07d43a17c67d2b3a49d424a7fb35757dea4e8
asicalug <isaac.poissinger@gmail.com>
2024-05-05 17:06:08 +0200

    Added a screenshot where omairr says he likes men

--- commands/admin/moderation/case.py
+++ commands/admin/moderation/case.py
@@ -42,11 +42,13 @@ class Case(commands.Cog):
         if user.id == 779442220104417280:
             avatarUrl = "https://cdn.discordapp.com/attachments/1231780683966582885/1236591328201015366/image.png?ex=663890eb&is=66373f6b&hm=538541765b9cd580515a7beccc6aab63798b12efc3e3e33b06196723261dfe7d&"
             description = "As you can see on the thumbnail, omairr **clearly** stated that he likes men which ultimately mean that he's gay."
-            imageUrl = "https://cdn.discordapp.com/attachments/1231780683966582885/1236591328201015366/image.png?ex=663890eb&is=66373f6b&hm=538541765b9cd580515a7beccc6aab63798b12efc3e3e33b06196723261dfe7d&"
+            image1Url = "https://cdn.discordapp.com/attachments/1231780683966582885/1236591328201015366/image.png?ex=663890eb&is=66373f6b&hm=538541765b9cd580515a7beccc6aab63798b12efc3e3e33b06196723261dfe7d&"
+            image2Url = "https://cdn.discordapp.com/attachments/1111425481238134784/1236695301591531520/image.png?ex=6638f1c0&is=6637a040&hm=8d20d609dd9ff1352c65a55a15afb29f970e4366898c1d9085f078ff0dc2135a&"
         else:
             avatarUrl = user.avatar.url
             description = ""
-            imageUrl = None
+            image1Url = None
+            image2Url = None
 
         joinedWarns = "\n".join(warns)
         joinedMods = "\n".join(mods)
@@ -61,12 +63,12 @@ class Case(commands.Cog):
         self.pages = [
             Page(
                 embeds=[
-                    discord.Embed(title=f"{user.name}'s Current Warn{s}", description=f"{description}", color=discord.Color.yellow()).set_thumbnail(url=avatarUrl).set_image(url=imageUrl).add_field(name="User", value=joinedUsers).add_field(name="Moderator", value=joinedMods).add_field(name="Reason", value=joinedWarns).add_field(name=f"ID{s}", value=joinedIds),
+                    discord.Embed(title=f"{user.name}'s Current Warn{s}", description=f"{description}", color=discord.Color.yellow()).set_thumbnail(url=avatarUrl).set_image(url=image1Url).add_field(name="User", value=joinedUsers).add_field(name="Moderator", value=joinedMods).add_field(name="Reason", value=joinedWarns).add_field(name=f"ID{s}", value=joinedIds),
                 ],
             ),
             Page(
                 embeds=[
-                    discord.Embed(title=f"{user.name}'s Current Mute{s}", description=f"{description}", color=discord.Color.yellow()).set_thumbnail(url=avatarUrl).set_image(url=imageUrl)
+                    discord.Embed(title=f"{user.name}'s Current Mute{s}", description=f"{description}", color=discord.Color.yellow()).set_thumbnail(url=avatarUrl).set_image(url=image2Url)
                 ]
             )
         ]
