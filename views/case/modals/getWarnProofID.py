752993013e88e1a6948ce4775bf5b2ecb8f1ee66
asicalug <isaac.poissinger@gmail.com>
2024-05-07 21:02:19 +0200

    final fix.

--- views/case/modals/getWarnProofID.py
+++ views/case/modals/getWarnProofID.py
@@ -22,10 +22,10 @@ class getWarnProofID(discord.ui.Modal):
 
     async def callback(self, interaction: discord.Interaction):
         latestUserName = self.bot.settings.get(f"Cases.Latest")
-
         try:
-            await interaction.response.send_message(content=f"This is {latestUserName}'s #{self.children[0].value}'s warn proof", file=discord.File(f"images/proofs/warns/{latestUserName}_warn{self.children[0].value}"), ephemeral=True)
-        except:
+            await interaction.response.send_message(content=f"This is {latestUserName}'s #{self.children[0].value}'s warn proof", file=discord.File(f"images/proofs/warns/{latestUserName}_warn{self.children[0].value}.png"), ephemeral=True)
+        except Exception as e:
+            print(e)
             embed = discord.Embed(title="Error!", description=f"No warns have been found with the #{self.children[0].value} id.\n Try again.", color=discord.Color.red())
             await interaction.response.send_message(embed=embed, ephemeral=True)
             
